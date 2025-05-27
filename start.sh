317abd49062e5b7ab72cb7f5b4cd6692d3c85518
Ceress <149852539+Khoirulmizan@users.noreply.github.com>
2025-05-27 21:04:10 +0700

    Updated Shell Code for Docker Run

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker run --rm -t --name bigdata-ecommerce --hostname localhost -P -p 9000:9000 -p 9866:9866 -p 9870:9870 -p 8088:8088 -p 9000:9000 -p 8000:8000 -p 10000:10000 -p 10001:10001 -p 10002:10002 -p 50030:50030 -p 2181:2181 -p 2888:2888 -p 3888:3888 -p 3306:3306 -p 4040:4040 -it -d bigdata-ecommerce /bin/bash -c "/bootstrap.sh >/tmp/bootstrap.log"
---add-host=host.docker.internal:host-gateway
+docker run --rm -t --name bigdata-ecommerce --hostname localhost --add-host=host.docker.internal:host-gateway -P -p 9000:9000 -p 9866:9866 -p 9870:9870 -p 8088:8088 -p 9000:9000 -p 8000:8000 -p 10000:10000 -p 10001:10001 -p 10002:10002 -p 50030:50030 -p 2181:2181 -p 2888:2888 -p 3888:3888 -p 3306:3306 -p 4040:4040 -it -d bigdata-ecommerce /bin/bash -c "/bootstrap.sh >/tmp/bootstrap.log"
